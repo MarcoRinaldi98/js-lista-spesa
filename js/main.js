8ab8dd1fc0ef18b1a422db1b1c66843f510d640f
Marco Rinaldi <marcorinaldi98@libero.it>
2023-03-01 13:07:54 +0100

    Esercizio completato

--- js/main.js
+++ js/main.js
@@ -2,3 +2,28 @@
     JAVASCRIPT
 */
 
+//array
+elementiLista = [
+    'Latte',
+    'Pane',
+    'Acqua',
+    'Banane',
+    'Mele',
+    'Merendine',
+    'Prosciutto',
+    'Carne',
+    'Pasta',
+    'Sapone'
+];
+
+
+const listDom = document.getElementById('list');
+
+let i = 0;
+
+while (i < elementiLista.length) {
+
+    listDom.innerHTML += `<li class="list-group-item d-flex justify-content-between">${elementiLista[i]} <input type="checkbox"> </li>`;
+
+    i++;
+}
